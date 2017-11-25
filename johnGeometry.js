7043595cbd06aa98b1414a5529d2a697278fefe4
JohnFriedman <john.friedman@uconn.edu>
2017-11-24 23:56:12 -0500

    John did some good work fellas, check out his test files

--- johnGeometry.js
+++ johnGeometry.js
@@ -1,58 +1,67 @@
 var points = [];
-var canvas = document.getElementById('canvas');
-/*canvas.addEventListener("click", addPoint("onclick"));
-
 function addPoint(event)
 {
-	console.log("x : " + event.offsetX + " y: " + event.offsetY);
+	var x = event.offsetX;
+	var y = event.offsetY;
+	console.log("x: " + x + " y: " + y);
+	points.push({x: event.x, y: y});
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	context.fillStyle = 'red';
+	context.beginPath();
+	context.arc(x, y, 3, 2 * Math.PI, false);
+	context.fill();
+	context.stroke();
 }
-*/
-
-points.push({x: 30, y: 30});
-points.push({x: 300, y: 200});
-points.push({x: 200, y: 300});
-
-var lines = [];
-lines.push({p1: points[0], p2: points[1]});
-lines.push({p1: points[1], p2: points[2]});
-lines.push({p1: points[2], p2: points[0]});
-
-
-var context = canvas.getContext('2d');
-context.fillStyle = 'red';
-
-var iteration = 0;
-var timeOfLastDrawing = Date.now();
-window.requestAnimationFrame
-(
-	function loop() 
+function startSim()
+{
+	if(points.length >= 3)
 	{
-		while (Date.now() < timeOfLastDrawing + 500) 
-		{}
-	  	timeOfLastDrawing = Date.now();
-	  	iteration++;
-	  	context.clearRect(0, 0, 400, 400);
-	  	for (var i = 0; i < points.length; i++) 
+		var lines = [];
+		for(var p = 1; p < points.length; p++)
 		{
-			var current = points[i];
-			context.beginPath();
-			context.arc(current.x, current.y, 3, 2 * Math.PI, false);
-			context.fill();
-			context.stroke();
+			lines.push({p1: points[p - 1], p2: points[p]});
 		}
-		for (i = lines.length - 1; i >= 0; i--)
-		{
-			if (i !== iteration % lines.length) 
-			{ 
-				continue; 
+		lines.push({p1: points[points.length - 1], p2: points[0]});
+		var canvas = document.getElementById('canvas');
+		var context = canvas.getContext('2d');
+		context.fillStyle = 'red';
+		
+		var iteration = 0;
+		var timeOfLastDrawing = Date.now();
+		window.requestAnimationFrame
+		(
+			function loop() 
+			{
+				while (Date.now() < timeOfLastDrawing + 500) 
+				{}
+			  	timeOfLastDrawing = Date.now();
+			  	iteration++;
+			  	context.clearRect(0, 0, 400, 400);
+			  	for (var i = 0; i < points.length; i++) 
+				{
+					var current = points[i];
+					context.beginPath();
+					context.arc(current.x, current.y, 3, 2 * Math.PI, false);
+					context.fill();
+					context.stroke();
+				}
+				for (i = lines.length - 1; i >= 0; i--)
+				{
+					if (i !== iteration % lines.length) 
+					{ 
+						continue; 
+					}
+					var current = lines[i];
+					context.beginPath();
+					context.moveTo(current.p1.x, current.p1.y);   
+					context.lineTo(current.p2.x, current.p2.y);
+					context.stroke();
+				}
+				window.requestAnimationFrame(loop);
 			}
-			var current = lines[i];
-			context.beginPath();
-			context.moveTo(current.p1.x, current.p1.y);   
-			context.lineTo(current.p2.x, current.p2.y);
-			context.stroke();
-		}
-		window.requestAnimationFrame(loop);
+		)
 	}
-)
-
+	else
+		alert("Not enough points!");
+}
