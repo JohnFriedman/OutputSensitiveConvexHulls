538433454b2c88aa80d8733ebfc0f1f9a704d3a6
JohnFriedman <john.friedman@uconn.edu>
2017-11-29 12:53:39 -0500

    11/29 John update geometry

--- johnGeometry.js
+++ johnGeometry.js
@@ -17,47 +17,59 @@ function addPoint(event)
 	context.stroke();
 }
 
+var lines = [];
+var numLines = 0;
+
 function startSim()
 {
-	
-	if(points.length >= 3)
-	{
-		var lines = [];
-		var numLines = 0;
-		for(var p = 1; p < points.length; p++)
-		{
-			lines.push({p1: points[p - 1], p2: points[p]});
-		}
-		lines.push({p1: points[points.length - 1], p2: points[0]});
-		
-		var iteration = -1;
-		var timeOfLastDrawing = Date.now();
-		window.requestAnimationFrame
-		(
-			function loop() 
-			{
-				while (Date.now() < timeOfLastDrawing + 500) {}
-			  	timeOfLastDrawing = Date.now();
-			  	iteration++;
-			  	//context.clearRect(0, 0, 400, 400);
-				for (i = lines.length - 1; i >= 0; i--)
-				{
-					if (i !== iteration % lines.length) 
-					{
-						continue; 
-					}
-					var current = lines[i];
-					context.beginPath();
-					context.moveTo(current.p1.x, current.p1.y);   
-					context.lineTo(current.p2.x, current.p2.y);
-					context.stroke();
-					numLines++;
-				}
-				if(numLines < lines.length)
-					window.requestAnimationFrame(loop);
-			}
-		)
-	}
+	if(points.length < 3)
+		alert("Not enough points!")
 	else
+	{
+		organizeLines();
+		window.requestAnimationFrame(fullSim);
+	}
+}
+
+function oneStep()
+{
+	if(points.length < 3)
 		alert("Not enough points!");
+	else
+	{
+		if(lines.length === 0)
+			organizeLines();
+		if(numLines === lines.length)
+			alert("All lines drawn!");
+		else
+			window.requestAnimationFrame(next);
+	}
+}
+
+function organizeLines()
+{	
+	lines = [];
+	for(var p = 1; p < points.length; p++)
+		lines.push({p1: points[p-1], p2: points[p]});
+	lines.push({p1: points[points.length - 1], p2: points[0]});
+}
+
+function next()
+{
+	var current = lines[numLines];
+	context.beginPath();
+	context.moveTo(current.p1.x, current.p1.y);
+	context.lineTo(current.p2.x, current.p2.y);
+	context.stroke();
+	numLines++;
+}
+
+function fullSim()
+{
+	var timeLast = Date.now();
+	while(Date.now() < timeLast + 500) {}
+	timeLast = Date.now();
+	next();
+	if(numLines < lines.length)
+		window.requestAnimationFrame(fullSim);
 }
