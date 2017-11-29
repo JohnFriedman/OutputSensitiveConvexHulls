6c731e49acc7b1fd4deaf70dc3749b8f7ac2e4ae
JohnFriedman <john.friedman@uconn.edu>
2017-11-29 14:35:23 -0500

    Can generate random points, and run one step at a time or normal simulation

--- johnGeometry.js
+++ johnGeometry.js
@@ -2,15 +2,40 @@ var points = [];
 var canvas;
 var context;
 
-function addPoint(event)
+function generatePoints(n)
+{
+	if(n > 1000)
+		alert("Do not generate more than 1000 points. Even that is way too many")
+	else
+	{
+		canvas = document.getElementById('canvas');
+		context = canvas.getContext('2d');
+		context.fillStyle = 'red';
+		var width = canvas.width;
+		var height = canvas.height;
+		for(var i = 0; i < n; i++)
+		{
+			var x = 15 + Math.floor(Math.random() * (width - 30));
+			var y = 15 + Math.floor(Math.random() * (height - 30));
+			addPoint(x, y);
+		}
+	}
+}
+
+function clickPoint(event)
 {
 	var x = event.offsetX;
 	var y = event.offsetY;
 	console.log("x: " + x + " y: " + y);
-	points.push({x: x, y: y});
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
 	context.fillStyle = 'red';
+	addPoint(x, y);
+}
+
+function addPoint(x, y)
+{
+	points.push({x: x, y: y});
 	context.beginPath();
 	context.arc(x, y, 3, 2 * Math.PI, false);
 	context.fill();
